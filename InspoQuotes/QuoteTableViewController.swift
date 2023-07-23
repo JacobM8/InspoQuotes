2b4323ab06c86a303fd4e707ae9c396de049e1cf
jacobmorrison <iamjacobmorrison@gmail.com>
2023-07-23 17:47:26 -0400

    Give access to purchased content

--- InspoQuotes/QuoteTableViewController.swift
+++ InspoQuotes/QuoteTableViewController.swift
@@ -6,15 +6,13 @@
 //  Copyright © 2018 London App Brewery. All rights reserved.
 //
 
-// MARK: com.jacobmorrison.InspoQuotes.PremiumQuotes
-// This is the product id used when setting up the in-app purchases for the premium quotes
-
 import UIKit
 import StoreKit
 
 class QuoteTableViewController: UITableViewController, SKPaymentTransactionObserver {
     
-    let productId = "com.jacobmorrison.InspoQuotes.PremiumQuotes"
+    //let productId = "com.jacobmorrison.InspoQuotes.PremiumQuotes"
+    let productId = "Premium_Quotes"
     
     var quotesToShow = [
         "Our greatest glory is not in never falling, but in rising every time we fall. — Confucius",
@@ -33,21 +31,33 @@ class QuoteTableViewController: UITableViewController, SKPaymentTransactionObser
         "Your true success in life begins only when you make the commitment to become excellent at what you do. — Brian Tracy",
         "Believe in yourself, take on your challenges, dig deep within yourself to conquer fears. Never let anyone bring you down. You got to keep going. – Chantal Sutherland"
     ]
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        if isPurchased(){
+            showPremiumQuotes()
+        }
+    }
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // When you add a new protocol - SKPaymentTransactionObserver - and you want to use it's delegate method you have to declare
         // a class as the delegate. We want to declare the current class - QuoteTableviewController - as the delegate who is going
-        // to receive the messages from the SKPaymentTransactionObserver when the transaction status changes.
+        // to receive the messages from the SKPaymentTransactionObserver when the transaction status changes because it is implementing
+        // the required functions for the protocol.
         SKPaymentQueue.default().add(self)
-
+        
+        return true
     }
-
+    
     // MARK: - Table view data source
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-
+        
+        if isPurchased() {
+            return quotesToShow.count
+        }
+        
         return quotesToShow.count + 1
     }
     
@@ -58,17 +68,18 @@ class QuoteTableViewController: UITableViewController, SKPaymentTransactionObser
         if (indexPath.row < quotesToShow.count){
             cell.textLabel?.text = quotesToShow[indexPath.row]
             cell.textLabel?.numberOfLines = 0 // setting to 0 uses as many lines as neccessary, see documentation
+            cell.textLabel?.textColor = .white
+            cell.accessoryType = .none
         } else {
             cell.textLabel?.text = "Buy more quotes"
-            // 160,210,224
             cell.textLabel?.textColor = UIColor(#colorLiteral(red: 0.193, green: 0.51, blue: 0.75, alpha: 255))
             cell.accessoryType = .disclosureIndicator
         }
         
-
+        
         return cell
     }
-
+    
     // MARK: - Table view delegate methods
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
@@ -80,31 +91,63 @@ class QuoteTableViewController: UITableViewController, SKPaymentTransactionObser
     }
     
     // MARK: - In-app purchase methods
+    
     func buyPremiumQuotes() {
-        // need to make sure phone is enabled, no parental controls set
+        // Check to make sure phone is enabled, no parental controls set
         if SKPaymentQueue.canMakePayments() {
-            // can make payments
+            // Can make payments
             let paymentRequest = SKMutablePayment()
             paymentRequest.productIdentifier = productId
             SKPaymentQueue.default().add(paymentRequest)
-            
+            //print("User can make payments")
         } else {
-            // can't make payments
+            // Can't make payments
             print("User can't make payments.")
         }
         
     }
     
+    // Whenever any changes happen to the payment transactions the app will contact the QuoteTableViewController
+    // and find this delegate method to trigger and update.
     func paymentQueue(_ queue: SKPaymentQueue, updatedTransactions transactions: [SKPaymentTransaction]) {
         // It is possible to have multiple transations becuase multiple transactions can happen in the same queue
         for transaction in transactions {
             if transaction.transactionState == .purchased {
-                // payment successfull
-                print("payment successful")
+                // Payment successfull
+                print("Transaction successful")
+                showPremiumQuotes()
+                UserDefaults.standard.set(true, forKey: productId)
+                SKPaymentQueue.default().finishTransaction(transaction)
             } else if transaction.transactionState == .failed {
-                // payment failed or user cancelled payment
-                print("payment failed")
+                // Payment failed or user cancelled payment
+                
+                if let error = transaction.error {
+                    let errorDescription = error.localizedDescription
+                    print("Payment failed due to error: \(errorDescription)")
+                }
+                
+                SKPaymentQueue.default().finishTransaction(transaction)
             }
+        }
+    }
+    
+    func showPremiumQuotes(){
+        
+        quotesToShow.append(contentsOf: premiumQuotes)
+        tableView.reloadData()
+    }
+    
+    func isPurchased() -> Bool {
+        let purchaseStatus = UserDefaults.standard.bool(forKey: productId)
+        
+        if purchaseStatus {
+            print("previously purchased")
+            
+            return true
+        } else {
+            print("never purchased")
+            
+            return false
         }
     }
     
